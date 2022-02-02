ce31915ecce63476e2ccdeb6eca63810794ebfe5
MauricioMalvaez <96149810+MauricioMalvaez@users.noreply.github.com>
2022-02-02 17:18:35 -0600

    Add files via upload

---
+++ loginOperators/src/loginOperators/logicOperators.java
@@ -0,0 +1,18 @@
+package loginOperators;
+
+import java.util.Scanner;
+
+public class logicOperators {
+public static void main(String[] args) {
+	Scanner console = new Scanner(System.in);
+	
+	System.out.println("Ingresa calificación: ");
+	int grade = console.nextInt();
+	
+	if(grade==10) {
+		System.out.println("Excelent");
+	
+
+}
+}
+}
